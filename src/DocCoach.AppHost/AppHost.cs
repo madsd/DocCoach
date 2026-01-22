eb10d82c37ababf54e91d8bdeffedac5a360a9ef
Mads Wolff Damgård <madsd@microsoft.com>
2026-01-22 13:38:58 +0100

    Remove Dev Tunnel configuration

--- src/DocCoach.AppHost/AppHost.cs
+++ src/DocCoach.AppHost/AppHost.cs
@@ -4,13 +4,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 // No need to add Azurite as an Aspire resource since it's managed externally
 
 // Add the web application (connects to Azurite directly via connection string)
-var web = builder.AddProject<Projects.DocCoach_Web>("web")
+builder.AddProject<Projects.DocCoach_Web>("web")
     .WithExternalHttpEndpoints();
-
-// Add dev tunnel for remote testing - exposes the web app publicly
-// The tunnel URL will be shown in the Aspire dashboard
-builder.AddDevTunnel("tunnel")
-    .WithReference(web)
-    .WithAnonymousAccess();
 
 builder.Build().Run();
